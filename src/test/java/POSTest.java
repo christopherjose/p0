5282073f06c01e825e22c1eb535c2941d502dfed
Christopher Jose <christopherjose2015@u.northwestern.edu>
2021-07-28 08:46:54 -0700

    project Zero

---
+++ src/test/java/POSTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test; //so you don't have to have fully qualified name in annotations
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+
+public class POSTest {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    @Test
+    void runMainMenu() {
+    }
+
+    @Test
+    void runOrderMenu() {
+    }
+
+/**
+    public input(InputStream in, PrintStream out) {
+        Scanner scanner = new Scanner(in);
+    }
+ **/
+}
+
