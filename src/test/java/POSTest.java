6a9e4c582896cd6066034ba9ee07fae4e715720a
Christopher Jose <christopherjose2015@u.northwestern.edu>
2021-08-04 19:06:44 -0700

    newest files

--- src/test/java/POSTest.java
+++ src/test/java/POSTest.java
@@ -2,28 +2,80 @@ import org.junit.jupiter.api.Test; //so you don't have to have fully qualified n
 
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class POSTest {
 
-    public static void main(String[] args) {
+    static HashMap<String, OrderProduct> products = new HashMap<String, OrderProduct>();
+
+    //Test checks SQL Query for pulling Sales Order Products for Order s0000
+    @Test
+    public void displaySQLOrderProdResults() {
+        System.out.println("Queried Order Product Information:");
+        System.out.println("");
+        SalesOrder salesOrder;
+        HashMap<String, OrderProduct> products = new HashMap<String, OrderProduct>();
+
+        HashMap<String, OrderProduct> orderProducts = new HashMap<String, OrderProduct>();
+        orderLookupQuery findOrder = new orderLookupQuery();
+        productLookupQuery findProduct = new productLookupQuery();
+        Map<String, SalesOrder> selectedOrder;
+        System.out.println("------------QUERY 1 START----------");
+        selectedOrder = findOrder.orderQuery("s0000");
+        orderProducts = findProduct.productQuery("s0000");
+        for (String k : orderProducts.keySet()) {
+            System.out.println(orderProducts.get(k).productName);
+        }
+        //TestClass.salesOrder.orderProductList= orderProducts;
+
+        products = orderProducts;
+
+        for (String k : orderProducts.keySet()) {
+            products.put(k, orderProducts.get(k));
+        }
+        System.out.println("");
+
 
     }
-
 
     @Test
-    void runMainMenu() {
+    void displaySQLCustomerResults() {
+        System.out.println("");
+        System.out.println("------------QUERY 2 START----------");
+        System.out.println("Queried Customer Information:");
+        CustomerLookupQuery CLQ = new CustomerLookupQuery();
+        CLQ.connectDatabase();
+        CustomerAccount result = CLQ.createCustomer("c0000");
+        System.out.println(result.customerId);
+        System.out.println(result.firstName);
+        System.out.println(result.lastName);
+        System.out.println(result.primaryPhoneNo);
+        System.out.println(result.emailAddress);
+        CLQ.close();
     }
 
     @Test
-    void runOrderMenu() {
+    void openStartMenu() {
+        System.out.println("------------QUERY 3 START----------");
+        POS menu = new POS();
+
     }
 
-/**
-    public input(InputStream in, PrintStream out) {
-        Scanner scanner = new Scanner(in);
+    @Test
+    void openOrderMenu() {
+        System.out.println("------------QUERY 4 START----------");
+        OrderMenu menu = new OrderMenu();
+        assertEquals(4,4);
     }
- **/
+
+
 }
+
 
